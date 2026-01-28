d7a3d8930d0ac2279c0122a93aa7f3fc444eb833
Lando <lando_vargas@outlook.com>
2026-01-28 20:23:55 -0300

    CH9 L3

---
+++ cli/evaluation_cli.py
@@ -0,0 +1,49 @@
+import argparse
+import json
+from lib.search_utils import GOLDEN_DATASET_PATH, RRF_K
+from lib.hybrid_search import rrf_search_command
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Search Evaluation CLI")
+    parser.add_argument(
+        "--limit",
+        type=int,
+        default=5,
+        help="Number of results to evaluate (k for precision@k, recall@k)",
+    )
+
+    args = parser.parse_args()
+    limit = args.limit
+
+    with open(GOLDEN_DATASET_PATH, "r", encoding="utf-8") as f:
+        data_set = json.load(f)
+
+    test_runs = []
+    for test in data_set.get("test_cases"):
+        results = rrf_search_command(test["query"], RRF_K, None, None, limit)
+        relevant = 0
+        for movie in results.get("results"):
+            if movie.get("title") in test["relevant_docs"]:
+                relevant += 1
+
+        run = {
+            "precision": (relevant / len(results.get("results"))),
+            "query": test["query"],
+            "retrieved": [m["title"] for m in results.get("results")],
+            "relevant": test["relevant_docs"],
+        }
+        test_runs.append(run)
+
+    print(f"k={limit}")
+    for run in test_runs:
+        print(f"  - Query: {run['query']}")
+        print(f"    - Precision@{limit}: {run['precision']:.4f}")
+        print(f"    - Retrieved: {', '.join(run['retrieved'])}")
+        print(f"    - Relevant: {', '.join(run['relevant'])}")
+
+    # run evaluation logic here
+
+
+if __name__ == "__main__":
+    main()
