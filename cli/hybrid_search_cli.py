d1ea4603c34a9327f1192bd555d4edccf8d6ecbe
Lando <lando_vargas@outlook.com>
2026-01-28 00:53:31 -0300

    CH7 L3

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,6 +1,9 @@
 import argparse
 from lib.hybrid_search import HybridSearch, normalize
 from lib.search_utils import load_movies
+import os
+from dotenv import load_dotenv
+from google import genai
 
 
 def main() -> None:
@@ -24,12 +27,16 @@ def main() -> None:
     )
     weighted_search_parser.add_argument("--limit", type=int, default=5, help="Limit")
 
-    rrf_search_parser = subparser.add_parser(
-        "rrf-search", help="Ranks results"
-    )
+    rrf_search_parser = subparser.add_parser("rrf-search", help="Ranks results")
     rrf_search_parser.add_argument("query", type=str, help="Query")
     rrf_search_parser.add_argument("--k", type=int, default=60, help="Weight")
     rrf_search_parser.add_argument("--limit", type=int, default=5, help="Limit")
+    rrf_search_parser.add_argument(
+        "--enhance",
+        type=str,
+        choices=["spell"],
+        help="Query enhancement method",
+    )
     args = parser.parse_args()
 
     match args.command:
@@ -51,8 +58,30 @@ def main() -> None:
                 print(result[1]["document"][:100])
         case "rrf-search":
             hs = HybridSearch(load_movies())
+            if args.enhance:
+                prompt = f"""Fix any spelling errors in this movie search query.
+
+Only correct obvious typos. Don't change correctly spelled words.
+
+Query: "{args.query}"
+
+If no errors, return the original query.
+Corrected:"""
+                load_dotenv()
+                api_key = os.environ.get("GEMINI_API_KEY")
+
+                client = genai.Client(api_key=api_key)
+                response = client.models.generate_content(
+                    model="gemini-2.5-flash",
+                    contents=prompt,
+                )
+
+                print(
+                    f"Enhanced query ({args.enhance}): '{args.query}' -> '{response.text}'\n"
+                )
+                args.query = response.text
             results = hs.rrf_search(args.query, args.k, args.limit)
-            results = results[:args.limit]
+            results = results[: args.limit]
 
             for index, result in enumerate(results):
                 print(f"{index + 1} {result[1]['title']}")
